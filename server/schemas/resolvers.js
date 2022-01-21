c24f5cb94b5d85a08e2bf726fdadfede99079b0c
NicoleWrz <nicolew96@gmail.com>
2022-01-21 14:06:31 -0500

    updated resolvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,38 +4,49 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    muscleGroup: async () => {
+    muscleGroups: async () => {
       return await MuscleGroup.find();
     },
-    exercises: async (parent, { muscleGroup, name }) => {
-      const params = {};
-      if (muscleGroup) {
-        params.muscleGroup = muscleGroup;
-      }
-      if (name) {
-        params.name = {
-          $regex: name,
-        };
-      }
-      return await Exercise.find(params).populate("muscleGroup");
+    exercises: async () => {
+      return await Exercise.find().populate('muscleGroup');
     },
-    exercises: async (parent, { _id }) => {
-        return await Exercise.findById(_id).populate('muscleGroup');
+    // exercises: async (parent, { muscleGroup, name }) => {
+    //   const params = {};
+    //   if (muscleGroup) {
+    //     params.muscleGroup = muscleGroup;
+    //   }
+    //   if (name) {
+    //     params.name = {
+    //       $regex: name,
+    //     };
+    //   }
+    //   return await Exercise.find(params).populate("muscleGroup");
+    // },
+    // exercise: async (parent, { _id }) => {
+    //   return await Exercise.findById(_id).populate("muscleGroup");
+    // },
+    // user: async (parent, args, context) => {
+    //   if (context.user) {
+    //     const user = await User.findById(context.user._id).populate({
+    //       path: "orders.products",
+    //       populate: "category",
+    //     });
+
+    //     user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+
+    //     return user;
+    //   }
+
+    //   throw new AuthenticationError("Not logged in");
+    // },
   },
-  user: async (parent, args, context) => {
-    if (context.user) {
-      const user = await User.findById(context.user._id).populate({
-        path: 'orders.products',
-        populate: 'category'
-      });
+  // Mutation: {
+  //   addUser: async (parent, args) => {
+  //     const user = await User.create(args);
+  //     const token = signToken(user);
 
-      user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
-      return user;
-    }
-
-    throw new AuthenticationError('Not logged in');
-  },
-    
-}};
+  //     return { token, user };
+  //   },
+  // }
+};
 module.exports = resolvers;
