7d712e468c3bbcf40b681367c06d3f316d264c2c
NicoleWrz <nicolew96@gmail.com>
2022-01-24 11:51:57 -0500

    updated resolvers, client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -7,11 +7,52 @@ export const QUERY_MUSCLEGROUPS = gql`
             name
         }
     }
-    `
+`
+export const QUERY_EXERCISES = gql`
+    {
+        exercises {
+            _id
+            name
+            reps
+            sets
+            rest
+            video
+            muscleGroup {
+                name
+            }
+        }
+    }
+`
+
+export const QUERY_ALL_USERS = gql`
+{
+    userWorkouts {
+        firstName
+        lastName
+        email
+        workout {
+            _id
+            name
+            exercises {
+                _id
+                name
+                video
+                reps
+                sets
+                rest
+                video
+                muscleGroup {
+                    name
+                }
+            }
+        }
+    }
+}
+`
 
 export const QUERY_ONE_USER = gql`
     {
-        user {
+        userWorkout {
             firstName
             lastName
             email
@@ -30,28 +71,6 @@ export const QUERY_ONE_USER = gql`
                         name
                     }
                 }
-            }
-        }
-    }
-`
-
-export const QUERY_ALL_USERS = gql`
-
-
-
-`
-
-export const QUERY_ALL_EXERCISES = gql`
-    {
-        exercises {
-            _id
-            name
-            reps
-            sets
-            rest
-            video
-            muscleGroup {
-                name
             }
         }
     }
