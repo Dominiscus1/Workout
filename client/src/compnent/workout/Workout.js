b56f06a9bc1b19f67f45736012b3863808a8e4e3
Anthony Jimenez <tony6776a@gmail.com>
2022-01-25 21:13:48 -0500

    Fixed workout.js, and queries so it displays exercises

--- client/src/compnent/workout/Workout.js
+++ client/src/compnent/workout/Workout.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./Workout.css";
-import WorkoutCard from "./workoutCard";
+import WorkoutCard from "./WorkoutCard";
 import { QUERY_EXERCISES, QUERY_MUSCLEGROUPS } from "../../utils/queries";
 import { useQuery, useLazyQuery } from "@apollo/client";
+
 
 function Workout() {
   console.log("Workout");
 
-  const {loading , data: muscleGroupData} = useQuery(QUERY_MUSCLEGROUPS);
+  const { loading, data: muscleGroupData } = useQuery(QUERY_MUSCLEGROUPS);
 
   const [getExercises, { data }] = useLazyQuery(QUERY_EXERCISES);
 
@@ -15,13 +16,24 @@ function Workout() {
 
   const muscleGroups = muscleGroupData?.muscleGroups;
 
+  
+
+
   useEffect(() => {
+    console.log(`useEfect - muscleGroup: ${muscleGroup}`);
+    //  get muscleGroup Id
     if (muscleGroup) {
+      const group = muscleGroups.filter( group => group.name === muscleGroup);
+      console.log(`useEffect - group: ${JSON.stringify(group)} `)
+      console.log(`useEffect - group[0]: ${JSON.stringify(group[0])} `);
+      console.log(`useEffect - group[0]._id: ${JSON.stringify(group[0]._id)} `);
+      // const groupId = group[0]._id;
+
       getExercises({
-        variables: { muscleGroup: muscleGroup },
+        variables: { muscleGroup: group[0]._id },
       });
     }
-  }, [muscleGroup]);
+  }, [muscleGroup, muscleGroups, getExercises]);
 
   if (loading) {
     return <div>loading ...</div>
@@ -29,14 +41,15 @@ function Workout() {
 
   return (
     <main>
+      {console.log(muscleGroups)}
       <select
         onChange={(e) => setMuscleGroup(e.target.value)}
         value={muscleGroup}
       >
         <option>Choose muscle group...</option>
-        {muscleGroups.map((muscleGroup) => (
-          <option value={muscleGroup}>
-            {muscleGroup}
+        {muscleGroups.map((group) => (
+          <option key={group._id} value={group.name}>
+            {group.name}
           </option>
         ))}
       </select>
@@ -45,19 +58,24 @@ function Workout() {
         <div className="col-12 col-md-8 mb-3">
           <div className="d__flex-row">
             {!data ? (
-              <div>loading ...</div>
+              <div>loading Exercises ...</div>
             ) : (
               data.exercises.map((exercise) => {
-                <WorkoutCard
-                  key={exercise._id}
-                  _id={exercise._id}
-                  name={exercise.name}
-                  video={exercise.video}
-                  reps={exercise.reps}
-                  sets={exercise.sets}
-                  rest={exercise.rest}
-                />;
+                console.log(exercise);
+                return (
+                  <WorkoutCard
+                    key={exercise._id}
+                    _id={exercise._id}
+                    name={exercise.name}
+                    video={exercise.video}
+                    reps={exercise.reps}
+                    sets={exercise.sets}
+                    rest={exercise.rest}
+                  />
+                )
               })
+
+
             )}
           </div>
         </div>
