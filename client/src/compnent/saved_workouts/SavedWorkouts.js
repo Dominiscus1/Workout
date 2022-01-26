358ca1152ee2ee747588379f669be6bd771eb728
QuynhDinh <quynhndinh96@gmail.com>
2022-01-26 15:20:15 -0500

    workout.js

--- client/src/compnent/saved_workouts/SavedWorkouts.js
+++ client/src/compnent/saved_workouts/SavedWorkouts.js
@@ -19,9 +19,9 @@ const SavedWorkouts = ({ _id, name, video, reps, sets, rest }) => {
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">{name}</div>
     <p class="text-gray-700 text-base">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Reps: <input type="number" placeholder={reps} style={{width: "50px"}}></input></span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Sets: <input type="number" placeholder={sets} style={{width: "50px"}}></input></span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Rests: <input type="number" placeholder={rest} style={{width: "50px"}}></input></span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Reps: {reps}</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Sets: {sets} </span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Rests: {rest} </span>
     </p>
   </div>
 </div>
